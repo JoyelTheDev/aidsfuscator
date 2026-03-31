5ac4639a6c8f67f59703f464a009c55b323dfd6b
LvStrng <126511387+LvStrnggg@users.noreply.github.com>
2026-03-31 06:53:17 +0300

    cleanup updatechecker a little

--- src/main/java/dev/lvstrng/aidsfuscator/UpdateChecker.java
+++ src/main/java/dev/lvstrng/aidsfuscator/UpdateChecker.java
@@ -7,38 +7,40 @@ import java.io.*;
 import java.net.*;
 
 public class UpdateChecker {
-  private static String getLatestReleaseTag() throws IOException {
-    HttpURLConnection connection = (HttpURLConnection) new URL(
-      "https://api.github.com/repos/LvStrnggg/aidsfuscator/releases/latest"
-    ).openConnection();
-    connection.setRequestProperty("Accept", "application/vnd.github.v3+json");
-    connection.setRequestMethod("GET");
-    if (connection.getResponseCode() == HttpURLConnection.HTTP_OK) {
-      BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-      StringBuilder response = new StringBuilder();
-      String line;
-      while ((line = reader.readLine()) != null)
-        response.append(line);
-      reader.close();
+    private static String getLatestReleaseTag() throws IOException {
+        var connection = (HttpURLConnection) new URL(
+                "https://api.github.com/repos/LvStrnggg/aidsfuscator/releases/latest"
+        ).openConnection();
+        connection.setRequestProperty("Accept", "application/vnd.github.v3+json");
+        connection.setRequestMethod("GET");
 
-      return JsonParser.parseString(response.toString()).getAsJsonObject().get("tag_name").getAsString();
-    } else {
-      throw new IllegalArgumentException("Invalid status code");
+        if (connection.getResponseCode() != HttpURLConnection.HTTP_OK) {
+            throw new IllegalArgumentException("Invalid status code");
+        }
+
+        var reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        var response = new StringBuilder();
+
+        String line;
+        while ((line = reader.readLine()) != null)
+            response.append(line);
+
+        reader.close();
+        return JsonParser.parseString(response.toString()).getAsJsonObject().get("tag_name").getAsString();
     }
-  }
 
-  public static void checkAndPrintUpdates() {
-     try {
-       String latest = getLatestReleaseTag();
-       String current = AidsfuscatorInfo.build();
-       if (!latest.equals(current)) {
-         Logger.warn("You're using an outdated version of Aidsfuscator, current version: %s, latest version: %s",
-           current, latest);
-       } else {
-         Logger.info("Everything is up to date. Current version: %s", AidsfuscatorInfo.versionText());
-       }
-     } catch (IOException e) {
-       Logger.error("There was an error checking for the latest version of Aidsfuscator", e);
-     }
-  }
+    public static void checkAndPrintUpdates() {
+        try {
+            var latest = getLatestReleaseTag();
+            var current = AidsfuscatorInfo.build();
+            if (!latest.equals(current)) {
+                Logger.warn("You're using an outdated version of Aidsfuscator, current version: %s, latest version: %s", current, latest);
+                return;
+            }
+
+            Logger.info("Everything is up to date. Current version: %s", AidsfuscatorInfo.versionText());
+        } catch (IOException e) {
+            Logger.error("There was an error checking for the latest version of Aidsfuscator", e);
+        }
+    }
 }
