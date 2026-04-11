f5813511b7d4184d86dae4c9201e9a63aa328399
SpigotRCE <128710385+SpigotRCE@users.noreply.github.com>
2026-04-11 11:45:56 +0530

    warn on disabld frame compute

--- src/main/java/dev/lvstrng/aidsfuscator/config/ConfigLoader.java
+++ src/main/java/dev/lvstrng/aidsfuscator/config/ConfigLoader.java
@@ -34,6 +34,8 @@ public class ConfigLoader {
 
         if(configObject.get("computeFrames").getAsBoolean())
             context.computeFrames();
+        else
+            Logger.warn("\n------------------------\n You've disabled frame computation, you will not recieve any support. Enable it in config with computeFrames \n------------------------");
 
         // ---- TRANSFORMERS ----
         var transformers = configObject.get("transformers").getAsJsonObject();
