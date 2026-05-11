99eb0324e1c78ba754277f4d0118a9b844dec60a
LvStrng <126511387+LvStrnggg@users.noreply.github.com>
2026-05-11 23:35:48 +0300

    same thing with renaming stuff

--- src/main/java/dev/lvstrng/aidsfuscator/transform/impl/salt/MethodSaltTransformer.java
+++ src/main/java/dev/lvstrng/aidsfuscator/transform/impl/salt/MethodSaltTransformer.java
@@ -44,14 +44,14 @@ public class MethodSaltTransformer extends Transformer {
             if(Exclusions.METHOD_SALTING.excluded(clazz))
                 continue;
 
-            if(clazz.tree().stream().anyMatch(Exclusions.METHOD_SALTING::excluded))
-                continue;
-
             for(var method : clazz.methods()) {
                 if(Exclusions.METHOD_SALTING.excluded(method))
                     continue;
 
-                if(clazz.tree().stream().anyMatch(e -> Exclusions.METHOD_SALTING.excluded(e, method)))
+                if(clazz.tree().stream().anyMatch(e -> Exclusions.METHOD_SALTING.excluded(e) && e.hasMethodInTree(context, method)))
+                    continue;
+
+                if(clazz.tree().stream().anyMatch(e -> Exclusions.METHOD_SALTING.excluded(e, method) && e.hasMethodInTree(context, method)))
                     continue;
 
                 if(cantEditMethod(clazz, method, true))
