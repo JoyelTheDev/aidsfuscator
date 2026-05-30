d4f5e6cad67a349bff5f1d085e3021150bf2da1e
Jones <admin@jonesdev.xyz>
2026-05-30 14:20:25 +0200

    Implement `excludeLowSignificant` setting

--- src/main/java/dev/lvstrng/aidsfuscator/transform/impl/data/ints/IntegerEncryptTransformer.java
+++ src/main/java/dev/lvstrng/aidsfuscator/transform/impl/data/ints/IntegerEncryptTransformer.java
@@ -3,6 +3,7 @@ package dev.lvstrng.aidsfuscator.transform.impl.data.ints;
 import dev.lvstrng.aidsfuscator.context.Context;
 import dev.lvstrng.aidsfuscator.exclude.impl.Exclusions;
 import dev.lvstrng.aidsfuscator.property.Property;
+import dev.lvstrng.aidsfuscator.transform.Setting;
 import dev.lvstrng.aidsfuscator.transform.Transformer;
 import dev.lvstrng.aidsfuscator.transform.impl.data.ints.decryptors.DefaultIntegerDecryptor;
 import dev.lvstrng.aidsfuscator.transform.impl.data.ints.initializers.DefaultIntegerInitializer;
@@ -23,6 +24,7 @@ public class IntegerEncryptTransformer extends Transformer {
     private final List<Supplier<IIntegerDecryptor>> decryptors = List.of(
             DefaultIntegerDecryptor::new
     );
+    private final Setting<Boolean> excludeLowSignificant = setting("excludeLowSignificant", true);
 
     public IntegerEncryptTransformer() {
         super("Encrypt Integer Constants", "integerEncrypt");
@@ -52,6 +54,9 @@ public class IntegerEncryptTransformer extends Transformer {
                 var frames = method.frames(context);
                 for(var insn : method.insns()) {
                     if(!ASMUtils.isIntPush(insn))
+                        continue;
+
+                    if(ASMUtils.isIconst(insn) && excludeLowSignificant.value())
                         continue;
 
                     if(context.properties().get(insn).has(Property.IGNORE_INTEGER))
