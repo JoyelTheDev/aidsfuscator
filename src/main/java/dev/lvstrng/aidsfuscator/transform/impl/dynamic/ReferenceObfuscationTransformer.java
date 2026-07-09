ab6840d30da24960323ae752ad0e6ca67a4227f0
ssheera <75024786+ssheera@users.noreply.github.com>
2026-07-09 19:13:55 +0100

    refactor: simplify character generation

--- src/main/java/dev/lvstrng/aidsfuscator/transform/impl/dynamic/ReferenceObfuscationTransformer.java
+++ src/main/java/dev/lvstrng/aidsfuscator/transform/impl/dynamic/ReferenceObfuscationTransformer.java
@@ -221,17 +221,9 @@ public class ReferenceObfuscationTransformer extends Transformer {
     }
 
     private char[] getChars() {
-        var chars = new ArrayList<Character>();
-        for(char c = 0; c < (char) 0xffff; c++) {
-            chars.add(c);
-        }
-
         var out = new char[6];
-        Collections.shuffle(chars);
-        for(int i = 0; i < out.length; i++) {
-            out[i] = chars.get(i);
-        }
-
+        for(int i = 0; i < out.length; i++)
+            out[i] = (char) random.nextInt(Character.MAX_VALUE + 1);
         return out;
     }
 }
