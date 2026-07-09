2f38a32ff8ed68f1f8ef3303f36061fa8e930872
LvStrng <126511387+LvStrnggg@users.noreply.github.com>
2026-07-09 04:37:57 +0300

    fix fat jars not being re-added after export

--- src/main/java/dev/lvstrng/aidsfuscator/context/pipeline/preprocess/impl/ArtifactImportProcessor.java
+++ src/main/java/dev/lvstrng/aidsfuscator/context/pipeline/preprocess/impl/ArtifactImportProcessor.java
@@ -47,10 +47,8 @@ public class ArtifactImportProcessor implements IProcessor {
                 }
 
                 // jars in a jar are "fat jars"
-                if(name.endsWith(".jar")) {
+                if(name.endsWith(".jar"))
                     context.libraryLoader().parseJar(bytes);
-                    continue;
-                }
 
                 // add resource
                 context.resourceHandler().add(name, bytes);
