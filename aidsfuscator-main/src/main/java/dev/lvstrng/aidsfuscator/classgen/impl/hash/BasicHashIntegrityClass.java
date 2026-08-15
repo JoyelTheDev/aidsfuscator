1e333c7508a4832ad49c5eae5e1ae6d5f132be50
LvStrng <126511387+LvStrnggg@users.noreply.github.com>
2026-08-16 01:55:26 +0300

    static init

--- aidsfuscator-main/src/main/java/dev/lvstrng/aidsfuscator/classgen/impl/hash/BasicHashIntegrityClass.java
+++ aidsfuscator-main/src/main/java/dev/lvstrng/aidsfuscator/classgen/impl/hash/BasicHashIntegrityClass.java
@@ -8,6 +8,8 @@ import dev.lvstrng.aidsfuscator.utils.InsnBuilder;
 import org.objectweb.asm.Type;
 import org.objectweb.asm.tree.LabelNode;
 
+import java.util.UUID;
+
 import static org.objectweb.asm.Opcodes.*;
 
 @SuppressWarnings("all")
@@ -16,9 +18,11 @@ public class BasicHashIntegrityClass implements IHashIntegrityClass {
     private JClass clazz;
     private JMethod retrieverMethod, hashMethod;
     private JField mapField, stringField, valueField;
+    private final String hashFileName;
 
     public BasicHashIntegrityClass(Context context) {
         this.context = context;
+        this.hashFileName = UUID.randomUUID().toString() + ".bin";
     }
 
     @Override
@@ -28,9 +32,127 @@ public class BasicHashIntegrityClass implements IHashIntegrityClass {
         this.stringField = clazz.createField(ACC_PRIVATE, context.dictionary().newFieldName(clazz, "Ljava/lang/String;"), "Ljava/lang/String;");
         this.valueField = clazz.createField(ACC_PRIVATE, context.dictionary().newFieldName(clazz, "I"), "I");
 
+        this.createStaticInitializer();
         this.createInitializer();
         this.createHasher();
         this.createRetriever();
+    }
+
+    private void createStaticInitializer() {
+        var method = clazz.findOrCreateClinit();
+
+        var hashFileVar = method.allocVar();
+        var bytesVar = method.allocVar();
+        var iVar = method.allocVar(Type.INT_TYPE);
+        var hashBytesVar = method.allocVar();
+        var keyBytesVar = method.allocVar();
+        var hashStringVar = method.allocVar();
+
+        var loopLabel = new LabelNode();
+        var list = new InsnBuilder()
+                .label()
+                .type(NEW, "java/util/HashMap")
+                .dup()
+                .method(INVOKESPECIAL, "java/util/HashMap", "<init>", "()V")
+                .field(PUTSTATIC, clazz.name(), mapField.name(), mapField.desc())
+
+                .label()
+                ._const(Type.getObjectType(clazz.name()))
+                ._const("/" + hashFileName)
+                .method(INVOKEVIRTUAL, "java/lang/Class", "getResourceAsStream", "(Ljava/lang/String;)Ljava/io/InputStream;")
+                ._var(ASTORE, hashFileVar)
+
+                .label()
+                ._var(ALOAD, hashFileVar)
+                .method(INVOKEVIRTUAL, "java/io/InputStream", "readAllBytes", "()[B")
+                ._var(ASTORE, bytesVar)
+
+                .label()
+                ._int(0)
+                ._var(ISTORE, iVar)
+
+                .label(loopLabel)
+                ._var(ALOAD, bytesVar)
+                ._var(ILOAD, iVar)
+                ._var(ILOAD, iVar)
+                ._int(32)
+                .iadd()
+                .method(INVOKESTATIC, "java/util/Arrays", "copyOfRange", "([BII)[B")
+                ._var(ASTORE, hashBytesVar)
+
+                .label()
+                ._var(ALOAD, bytesVar)
+                ._var(ILOAD, iVar)
+                ._int(32)
+                .iadd()
+                ._var(ILOAD, iVar)
+                ._int(36)
+                .iadd()
+                .method(INVOKESTATIC, "java/util/Arrays", "copyOfRange", "([BII)[B")
+                ._var(ASTORE, keyBytesVar)
+
+                .label()
+                .type(NEW, "java/lang/String")
+                .dup()
+                ._var(ALOAD, hashBytesVar)
+                .method(INVOKESPECIAL, "java/lang/String", "<init>", "([B)V")
+                .method(INVOKEVIRTUAL, "java/lang/String", "intern", "()Ljava/lang/String;")
+                ._var(ASTORE, hashStringVar)
+
+                .label()
+                .field(GETSTATIC, clazz.name(), mapField.name(), mapField.desc())
+                ._var(ALOAD, hashStringVar)
+                .type(NEW, clazz.name())
+                .dup()
+                ._var(ALOAD, hashStringVar)
+
+                ._var(ALOAD, keyBytesVar)
+                ._int(0)
+                .baload()
+                ._int(0xff)
+                .iand()
+                ._int(24)
+                .ishl()
+                ._var(ALOAD, keyBytesVar)
+                ._int(1)
+                .baload()
+                ._int(0xff)
+                .iand()
+                ._int(16)
+                .ishl()
+                .ior()
+                ._var(ALOAD, keyBytesVar)
+                ._int(2)
+                .baload()
+                ._int(0xff)
+                .iand()
+                ._int(8)
+                .ishl()
+                .ior()
+                ._var(ALOAD, keyBytesVar)
+                ._int(3)
+                .baload()
+                ._int(0xff)
+                .iand()
+                .ior()
+                .method(INVOKESPECIAL, clazz.name(), "<init>", "(Ljava/lang/String;I)V")
+                .method(INVOKEINTERFACE, "java/util/Map", "put", "(Ljava/lang/Object;Ljava/lang/Object;)Ljava/lang/Object;")
+                .pop()
+
+                .label()
+                .iinc(iVar, 36)
+
+                .label()
+                ._var(ILOAD, iVar)
+                ._var(ALOAD, bytesVar)
+                .arraylength()
+                .jump(IF_ICMPNE, loopLabel)
+
+                .label()
+                ._return()
+                ;
+
+        method.insns().insert(list.result());
     }
 
     private void createInitializer() {
